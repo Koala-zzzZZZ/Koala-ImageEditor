f3a8a21c0f4cb324ba697a3cbe00b42af9a060b6
杨思宇 <yangsiyuwork@163.com>
2018-03-10 09:45:45 +0800

    init

---
+++ Koala-ImageEditor/Koala-ImageEditor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Koala-ImageEditor
+//
+//  Created by Koala_zzzZZZ on 2018/3/10.
+//  Copyright © 2018年 Koala_zzzZZZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Koala-ImageEditor/Koala-ImageEditor/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Koala-ImageEditor
+//
+//  Created by Koala_zzzZZZ on 2018/3/10.
+//  Copyright © 2018年 Koala_zzzZZZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
